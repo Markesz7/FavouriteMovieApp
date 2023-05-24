c36de9a6d5de8273e33ea4f8ded73de9847336dc
Mark Martin Kaposvari <61698307+Markesz7@users.noreply.github.com>
2023-05-24 23:18:34 +0200

    Init spring boot project

---
+++ src/main/kotlin/hu/bme/aut/favouritemovieapp/FavouritemovieappApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.favouritemovieapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FavouritemovieappApplication
+
+fun main(args: Array<String>) {
+	runApplication<FavouritemovieappApplication>(*args)
+}
---
+++ src/test/kotlin/hu/bme/aut/favouritemovieapp/FavouritemovieappApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.favouritemovieapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FavouritemovieappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
